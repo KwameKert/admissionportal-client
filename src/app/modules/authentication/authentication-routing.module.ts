3222b2fb701a16b63a77d8e215c3707b51a25ca7
kwamkert <tomriddle477@gmail.com>
2020-05-17 11:22:46 +0000

    git add .

--- src/app/modules/authentication/authentication-routing.module.ts
+++ src/app/modules/authentication/authentication-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { ApplicantDetailComponent } from './components/applicant-detail/applicant-detail.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [ 
 
   {path: '', component: LoginComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'applicant_details/:id', component: ApplicantDetailComponent}
+  {path: 'applicant_details/:id', component: ApplicantDetailComponent},
+  {path: 'register', component: RegisterComponent},
   
 ];
 
