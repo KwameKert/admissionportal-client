2d2879176178e4da4306236c0991ce0db214e044
kwamkert <tomriddle477@gmail.com>
2020-05-19 16:57:42 +0000

    enviroment variables updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-
+  api_host: 'http://localhost:3000',
   image_client_id: '4107242f9c6bff2',
   image_client_secret: 'd6ce2d3a484a6981e365ad11fd55d47dc172cb64'
 };
